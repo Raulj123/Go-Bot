3bf4d64e2c64a352187c7777c934bb7b6bdccbe2
IamRaul <jarquinr121@gmail.com>
2023-05-02 22:27:40 -0700

    embed msg

--- main.go
+++ main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
 )
+
+const prefix string = "!gob"
 
 func main() {
 	// export DISCORD_BOT_TOKEN=blah
@@ -21,8 +25,32 @@ func main() {
 		if m.Author.ID == s.State.User.ID {
 			return
 		}
-
-		if m.Content == "hello" {
+		args := strings.Split(m.Content, " ")
+		if args[0] != prefix {
+			return
+		} else if args[1] == "proverbs" {
+			proverbs := []string{
+				"Don't communicate by sharing memory, share memory by communicating.",
+				"Concurrency is not parallelism.",
+				"Channels orchestrate; mutexes serialize.",
+				"The bigger the interface, the weaker the abstraction.",
+				"Make the zero value useful.",
+				"interface{} says nothing.",
+				"Gofmt's style is no one's favorite, yet gofmt is everyone's favorite.",
+			}
+			selection := rand.Intn(len(proverbs))
+			author := discordgo.MessageEmbedAuthor{
+				Name: "Rob Pike",
+				URL:  "https://go-proverbs.github.io/",
+			}
+			embed := discordgo.MessageEmbed{
+				Title:  proverbs[selection],
+				Author: &author,
+			}
+			s.ChannelMessageSendEmbed(m.ChannelID, &embed)
+			//s.ChannelMessageSend(m.ChannelID, proverbs[selection])
+		}
+		if args[1] == "hello" {
 			s.ChannelMessageSend(m.ChannelID, "world")
 		}
 	})
